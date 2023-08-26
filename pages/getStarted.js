3150bb5dfb82a652784d9d2ae0147d1e08a2d97b
arshaan-abh <arshaan.git@gmail.com>
2023-08-26 03:54:03 +0330

    Add get started page

---
+++ pages/getStarted.js
@@ -0,0 +1,11 @@
+import SeoHead from "../components/Landing/SeoHead";
+import Layout from "../components/Landing/Layout/Layout";
+
+export default function GetStarted() {
+    return <>
+        <SeoHead title="AragonVPN - Get Started"/>
+        <Layout>
+
+        </Layout>
+    </>
+}
