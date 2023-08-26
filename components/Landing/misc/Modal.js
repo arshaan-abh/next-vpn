7044c349a5f7797238a9cfceeea08af7b76dc1d3
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2023-08-26 12:17:05 +0330

    Formating fix

--- components/Landing/misc/Modal.js
+++ components/Landing/misc/Modal.js
@@ -1,11 +1,15 @@
-import React from "react"
-import styles from "../../../styles/modal.module.css"
+import React from "react";
+import styles from "../../../styles/modal.module.css";
 
-export default function Modal({closeModalHandler}) {
-    return <div className={styles.modalContainer}>
-        <div className={styles.modal}>
-            <div className={styles.closeModalButton} onClick={closeModalHandler}>X</div>
-            modal
-        </div>
-    </div>
+export default function Modal({ closeModalHandler }) {
+	return (
+		<div className={styles.modalContainer}>
+			<div className={styles.modal}>
+				<div className={styles.closeModalButton} onClick={closeModalHandler}>
+					X
+				</div>
+				modal
+			</div>
+		</div>
+	);
 }
